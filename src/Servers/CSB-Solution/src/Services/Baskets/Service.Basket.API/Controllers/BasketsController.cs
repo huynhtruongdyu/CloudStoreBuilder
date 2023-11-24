55d3082911cd7e2b1493f8bbf257ca92a172a839
Aaron <huynhtruongdyu@gmail.com>
2023-11-24 17:58:10 +0700

    first commit

---
+++ src/Servers/CSB-Solution/src/Services/Baskets/Service.Basket.API/Controllers/BasketsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Service.Basket.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class BasketsController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetBaskets()
+    {
+        return Ok(nameof(GetBaskets));
+    }
+}
---
+++ src/Servers/CSB-Solution/src/Services/Catalogs/Service.Catalog.API/Controllers/CatalogsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Service.Catalog.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class CatalogsController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetCatalogs()
+    {
+        return Ok(nameof(GetCatalogs));
+    }
+}
