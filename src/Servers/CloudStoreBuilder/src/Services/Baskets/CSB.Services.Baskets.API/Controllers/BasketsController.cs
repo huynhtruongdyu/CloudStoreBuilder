aa0d644c059514969f8e05cb2b5bd6d6af663346
Aaron <huynhtruongdyu@gmail.com>
2023-11-27 00:56:29 +0700

    add serilog baskets service

--- src/Servers/CloudStoreBuilder/src/Services/Baskets/CSB.Services.Baskets.API/Controllers/BasketsController.cs
+++ src/Servers/CloudStoreBuilder/src/Services/Baskets/CSB.Services.Baskets.API/Controllers/BasketsController.cs
@@ -3,11 +3,16 @@
 namespace CSB.Services.Baskets.API.Controllers;
 [Route("api/[controller]")]
 [ApiController]
-public class BasketsController : ControllerBase
-{
+public class BasketsController : ControllerBase {
+    private ILogger<BasketsController> _logger;
+
+    public BasketsController(ILogger<BasketsController> logger) {
+        _logger = logger;
+    }
+
     [HttpGet]
-    public IActionResult GetBaskets()
-    {
+    public IActionResult GetBaskets() {
+        _logger.LogInformation("hello world");
         return Ok(nameof(GetBaskets));
     }
 }
--- src/Servers/CloudStoreBuilder/src/Services/Baskets/CSB.Services.Baskets.API/Program.cs
+++ src/Servers/CloudStoreBuilder/src/Services/Baskets/CSB.Services.Baskets.API/Program.cs
@@ -1,23 +1,40 @@
-var builder = WebApplication.CreateBuilder(args);
+using Serilog;
 
-// Add services to the container.
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+try {
+    var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
+    #region Default services
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    #endregion Default services
+
+    builder.Host.UseSerilog((context, configuration) => {
+        configuration.ReadFrom.Configuration(context.Configuration);
+    });
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment()) {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+    app.UseSerilogRequestLogging();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+} catch (Exception ex) {
+    Log.Fatal(ex, "Application terminated unexpectedly");
+} finally {
+    Log.CloseAndFlush();
 }
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
