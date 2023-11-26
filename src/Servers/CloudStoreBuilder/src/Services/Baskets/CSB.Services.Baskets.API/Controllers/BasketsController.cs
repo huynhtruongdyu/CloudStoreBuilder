563f7ddf1f5b8009aeda86a32aedd7e9c39b9e25
Aaron <huynhtruongdyu@gmail.com>
2023-11-27 00:30:29 +0700

    refactor servers

---
+++ src/Servers/CloudStoreBuilder/src/Services/Baskets/CSB.Services.Baskets.API/Controllers/BasketsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CSB.Services.Baskets.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class BasketsController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetBaskets()
+    {
+        return Ok(nameof(GetBaskets));
+    }
+}
---
+++ src/Servers/CloudStoreBuilder/src/Services/Catalogs/CSB.Services.Catalogs.API/Controllers/CatalogsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CSB.Services.Catalogs.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class CatalogsController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetCatalogs()
+    {
+        return Ok(nameof(GetCatalogs));
+    }
+}
