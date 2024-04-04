a2672f564530e5f5865dce9894d747aa84ea6a84
aaron <huynhtruongdyu@gmail.com>
2024-04-04 15:05:33 +0700

    init

--- src/csb-client/src/app/app.component.ts
+++ src/csb-client/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SharedModule } from './shared/shared.module';
+import { ButtonComponent } from './shared/components/ui/button/button.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SharedModule, ButtonComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'csb-client';
